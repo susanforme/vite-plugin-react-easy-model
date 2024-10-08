3844f0d7da77660666569802efdfee09e01afdd1
susanforme <rzc307853639@gmail.com>
2024-10-08 15:31:48 +0800

    update

--- client.d.ts
+++ client.d.ts
@@ -1,7 +1,5 @@
-/*
- * @Author: susanforme
- * @Date: 2024-10-08 15:14:23
- * @LastEditTime: 2024-10-08 15:14:30
- * @FilePath: \vite-plugin-react-easy-model\client.d.ts
- * @Description: 
- */
+declare module '~react-model' {
+  // 导出any
+  let defaultModule:any;
+  export default defaultModule;
+}
